38bc324a6d5a3d851954f04cb74372fd03f01237
jojo228 <49480316+jojo228@users.noreply.github.com>
2024-10-09 14:56:25 +0000

    push

--- user_api/u_package_purchase.php
+++ user_api/u_package_purchase.php
@@ -4,7 +4,12 @@ require dirname(dirname(__FILE__)) . '/include/estate.php';
 header('Content-type: text/json');
 $data = json_decode(file_get_contents('php://input'), true);
 
-if ($data['uid'] == '' or $data['plan_id'] == '' or $data['transaction_id'] == '' or $data['pname'] == '') {
+if (
+    !isset($data['uid']) ||
+    !isset($data['plan_id']) ||
+    !isset($data['transaction_id']) ||
+    !isset($data['pname'])
+) {
     $returnArr = array(
         "ResponseCode" => "401",
         "Result" => "false",
